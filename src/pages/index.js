c10d1c507f4a60f7ad0b9c197b6f8a6efd12f61a
oshoubber <oshoubber@gmail.com>
2022-03-30 15:42:19 -0700

    Hi to Hey

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Index = () => {
       <NavBar />
       <Center className={styles.HomeTextName}>
         <Text w="100%" fontSize="8xl" fontWeight="bold">
-          Hi, I'm <span style={{color: '#38B2AC'}}>Osama</span>
+          Hey, I'm <span style={{color: '#38B2AC'}}>Osama</span>
         </Text>
       </Center>
       <Particles/>
